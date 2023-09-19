0fa65d6f0e894525bc47e0133a617bbff57d413e
IsaureStiffel <zoziore.sti@gmail.com>
2023-09-19 11:40:11 +0200

    google

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' YOUR_GA_CODE');
+</script>
+ """
+ return prefix_google + "Hello World"
