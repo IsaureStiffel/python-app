2caab3515bb515696ac6b142f0eda9943d21a905
IsaureStiffel <zoziore.sti@gmail.com>
2023-10-13 17:41:31 +0200

    Final Lab2

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import logging
+import requests
+
+from google.auth.transport.requests import Request
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route('/fetch-google-analytics-data', methods=['GET'])
+def fetch_google_analytics_data():
+    SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+    SERVICE_ACCOUNT_FILE = 'datasourcelab2-7fabd9746820.json'
+
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    
+    service = build('analytics', 'v3', credentials=credentials)
+
+    # Acess the google analytics data
+    def get_visitor_count(service):
+        response = service.data().realtime().get(
+            ids='ga:407471832',
+            metrics='ga:activeVisitors',
+        ).execute()
+        return response
+
+    # Retreive info about the number of visitor
+    visitor_data = get_visitor_count(service)
+    visitor_count = visitor_data.get('rows', [])[0][0]
+
+    return f'Nombre de visiteurs actifs : {visitor_count}'
+
 
 @app.route("/")
 def hello_world():
@@ -16,3 +49,92 @@ src="https://www.googletagmanager.com/gtag/js?id=G-E44BHZD8C7"></script>
 </script>
  """
  return prefix_google + "Hello World"
+
+@app.route("/logger", methods=['GET', 'POST'])
+def logger():
+    # Log on the Python side (server-side)
+    python_log = "This is a python log."
+    app.logger.info("This is a python log.")
+
+    # Log on the browser (client-side) using JavaScript
+    browser_log = """
+    <script>
+        console.log("This is a browser log.");
+    </script>
+    """
+
+    if request.method == 'POST':
+        # Récupérer le texte entré dans la boîte de texte
+        text_from_textbox = request.form['textbox']
+
+        # Print a message in the browser console with the text from the text box
+        browser_log = f"""
+        <script>
+            console.log('Web browser consol: You are connected to a log page');
+            console.log('Text from the text box : {text_from_textbox}');
+        </script>
+        """
+    else:
+        # Print a message in the browser console
+        browser_log = """
+        <script>
+            console.log('Web browser consol: You are connected to a log page');
+        </script>
+        """
+
+    # Formulaire HTML avec une boîte de texte and a button
+    form = """
+    <form method="POST">
+        <label for="textbox">Text Box :</label><br>
+        <input type="text" id="textbox" name="textbox"><br><br>
+        <input type="submit" value="Soumettre">
+    </form>
+    """
+    return python_log + browser_log + form
+
+@app.route('/google-request', methods=['GET'])
+def google_request():
+    # Render a form with a button to make the Google request
+    return """
+    <form method="GET" action="/perform-google-request">
+        <input type="submit" value="Make Google Request">
+    </form>
+    <form method="GET" action="/perform-google-request-cookies">
+        <input type="submit" value="Check Google Analytics Request Cookies">
+    </form>
+    """
+
+@app.route('/perform-google-request', methods=['GET'])
+def perform_google_request():
+    google_url = "https://www.google.com/"
+    google_analytics_url = "https://analytics.google.com/analytics/web/#/p407471832/reports/home"
+
+    try:
+        response = requests.get(google_analytics_url)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+
+        return response.text
+    except requests.exceptions.RequestException as e:
+        return f"Error making Google Analytics request: {str(e)}"
+    
+@app.route('/perform-google-request-cookies', methods=['GET'])
+def perform_google_request_cookies():
+    google_url = "https://www.google.com/"
+    google_analytics_url = "https://analytics.google.com/analytics/web/#/p407471832/reports/home"
+
+    try:
+        response = requests.get(google_analytics_url)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+
+        # Retrieve cookies of the response
+        cookies = response.cookies
+
+        # Send cookies to the template for display
+        return render_template('cookies.html', cookies=cookies)
+            
+    except requests.exceptions.RequestException as e:
+        return f"Error making Google Analytics Cookies request: {str(e)}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
